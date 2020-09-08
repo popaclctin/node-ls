055aac18173f343172fc702e864059e2186527ba
Claudiu Popa <popa.clctin@gmail.com>
2020-09-08 17:02:02 +0300

    Read the contents of the provided dir and check each item if it's a file

--- index.js
+++ index.js
@@ -2,11 +2,35 @@
 
 const fs = require("fs");
 
-fs.readdir(process.cwd(), (err, fileNames) => {
+const { lstat } = fs.promises;
+
+fs.readdir(process.cwd(), async (err, fileNames) => {
   if (err) {
     console.log(err);
     return;
   }
 
-  console.log(fileNames);
+  const statPromises = fileNames.map((filename) => {
+    return lstat(filename);
+  });
+
+  const allStats = await Promise.all(statPromises);
+
+  for (let stat of allStats) {
+    const index = allStats.indexOf(stat);
+
+    console.log(fileNames[index], stat.isFile());
+  }
 });
+
+// const lstat = (filename) => {
+//   return new Promise((resolve, reject) => {
+//     fs.lstat(filename, (err, stats) => {
+//       if (err) {
+//         reject(err);
+//       }
+
+//       resolve(stats);
+//     });
+//   });
+// };
